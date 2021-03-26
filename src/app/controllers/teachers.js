924df66b605785feee148448c9eb595411c985ce
Gustavo Azevedo <gustavosazevedo@gmail.com>
2021-03-26 20:26:46 -0300

    manipulating teachers with database

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,8 +1,14 @@
 const { age, graduation, date } = require("../../lib/utils")
+const Teacher = require("../models/Teacher")
 
 module.exports = {
   index(req, res) {
-    return res.render("teachers/index")
+    Teacher.all((teachers) => {
+      for (teacher of teachers) {
+        teacher.subjects_taught = teacher.subjects_taught.split(",")
+      }
+      return res.render("teachers/index", { teachers })
+    })
   },
   create(req, res) {
     return res.render("teachers/create")
@@ -15,15 +21,26 @@ module.exports = {
       }
     }
   
-    let { avatar_url, name, birth, education_level, type_class, classes } = req.body
-
-    return
+    Teacher.create(req.body, (teacher) => {
+      return res.redirect(`/teachers/${teacher.id}`)
+    })
   },
   show(req, res) {
-    return
+    Teacher.find(req.params.id, (teacher) => {
+      teacher.age = age(teacher.birth_date)
+      teacher.education_level = graduation(teacher.education_level)
+      teacher.subjects_taught = teacher.subjects_taught.split(",")
+      teacher.created_at = date(teacher.created_at).birthDay
+
+      return res.render("teachers/show", { teacher })
+    })
   },
   edit(req, res) {
-    return
+    Teacher.find(req.params.id, (teacher) => {
+      teacher.birth_date = date(teacher.birth_date).iso
+
+      return res.render(`teachers/edit`, { teacher })
+    })
   },
   update(req, res) {
     const keys = Object.keys(req.body)
@@ -33,9 +50,13 @@ module.exports = {
       }
     }
 
-    return
+    Teacher.update(req.body, () => {
+      return res.redirect(`teachers/${req.body.id}`)
+    })
   },
   delete(req, res) {
-    return
+    Teacher.delete(req.body.id, () => {
+      return res.redirect("teachers")
+    })
   }
 }
